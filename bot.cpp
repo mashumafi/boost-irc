c236de38323ac2ce528250d88ef587a57ec3b7c4
Matthew Murphy <nonplayablecharacter@hotmail.com>
2016-07-29 02:19:29 +0000

    added http server

--- bot.cpp
+++ bot.cpp
@@ -1,19 +1,28 @@
 #include "twitch_client.hpp"
 
 #include <iostream>
+#include <server.hpp>
 
 int main(int argc, char* argv[]) {
-  if (argc != 3)
+  if (argc != 4)
   {
-    std::cerr << "Usage: bot username password\n";
+    std::cerr << "Usage: bot username password doc_root\n";
     return 1;
   }
   twitch_client irc(argv[1], argv[2]);
-  char line[64];
+  
+  /*char line[64];
   while (std::cin.getline(line, 64))
   {
     irc.send(line);
-  }
+  }*/
+  
+  // Initialise the server.
+  // <address> <port> <doc_root>
+  http::server::server s("0.0.0.0", "8000", argv[3]);
+
+  // Run the server until stopped.
+  s.run();
 
   return 0;
 }
